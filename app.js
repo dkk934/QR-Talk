5396309a73b6510145e4b03544c60a2e11d831ff
dkk934 <dkking934@gmail.com>
2024-04-08 19:21:08 +0530

    Encryption

--- app.js
+++ app.js
@@ -8,16 +8,47 @@ const port = process.env.PORT || 3000;
 app.use(body.urlencoded({ extended: true }))
 app.use(exp.static("public"));
 
+function encodeToMorse(text) {
+  var morseCodeMap = {
+      ' ':'/','A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+      'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+      'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+      'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+      'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+      'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+      '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+      '9': '----.',
+      '!': '!',
+      '@': '@',
+      '#': '#',
+      '$': '$',
+      '%': '%',
+      '^': '^',
+      '&': '&',
+      '*': '*'
+  };
+  text = text.toUpperCase();
+  var morseCode = '';
+  for (var i = 0; i < text.length; i++) {
+      var char = text[i];
+      if (morseCodeMap[char]) {
+          morseCode += morseCodeMap[char] + ' ';
+      } else if (char === ' ') {
+          morseCode += '  ';
+      }
+  }
+  return morseCode.trim();
+}
+
 app.get("/",(req,res)=>{
   res.render('index.ejs')
 })
 
 app.post("/qr",(req,res)=>{
     const url= req.body.text;
-    var qr_svg = qr.imageSync(url,{type : 'png'});
-    // console.log(qr_svg);
-    const buffer = qr_svg // your Buffer
-    // Convert the Buffer to a base64 string
+    const url2 = encodeToMorse(url)
+    var qr_svg = qr.imageSync(url2,{type : 'png'});
+    const buffer = qr_svg
     const base64 = buffer.toString('base64');
     res.render('index.ejs',{image : base64})
 })
